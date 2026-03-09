891518f90b86d6b4dd3975c793a21ed42e8d4030
DinaDwiLestari729 <dinadwilestari.9020@gmail.com>
2026-03-09 13:58:49 +0700

    update Main.java

--- latihan_2407047/src/latihan_pbo/inheritance_bangun_datar/Main.java
+++ latihan_2407047/src/latihan_pbo/inheritance_bangun_datar/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         
         persegi p = new persegi();
-        p.setSisi(8);
+        p.setSisi(5);
 
         System.out.println("Luas Persegi = " + p.luasPersegi());
         System.out.println("Keliling Persegi = " + p.kelilingPersegi());
@@ -15,5 +15,21 @@ public class Main {
 
         System.out.println("Luas Persegi Panjang = " + q.luasPersegiPanjang());
         System.out.println("Keliling Persegi Panjang = " + q.kelilingPersegiPanjang());
+
+        Segitiga s = new Segitiga();
+        s.setAlas(8);
+        s.setTinggi(6);
+        s.setkelilingSegitiga(3,4,5);
+
+        System.out.println("Luas Segitiga = "+ s.luasSegitiga());
+        System.out.println("Keliling Segitiga = "+ s.kelilingSegitiga());
+
+        BangunDatar bk = new BangunDatar();
+        bk.setDiagonal1(10);
+        bk.setDiagonal2(8);
+        bk.setSisiBelahKetupat(5);
+
+        System.out.println("Luas Belah Ketupat : " + bk.luasBelahKetupat(bk.getdiagonal1(), bk.getdiagonal2()));
+        System.out.println("Keliling Belah Ketupat : " + bk.kelilingBelahKetupat());
     }
 }
