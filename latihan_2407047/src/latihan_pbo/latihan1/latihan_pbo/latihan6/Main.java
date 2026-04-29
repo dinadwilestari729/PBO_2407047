e2047a3d49d72b4c5ae19216ff6f827006648492
DinaDwiLestari729 <dinadwilestari.9020@gmail.com>
2026-04-29 09:34:24 +0700

    Praktikum di kelas abstraction latihan6

---
+++ latihan_2407047/src/latihan_pbo/latihan1/latihan_pbo/latihan6/Main.java
@@ -0,0 +1,17 @@
+package latihan_pbo.latihan1.latihan_pbo.latihan6;
+
+public class Main {
+    public static void main(String[] args) {
+        BangunDatar[] bd = {
+            new Persegi(6),
+            new Segitiga(4, 6, 8)
+        };
+
+        for (BangunDatar b : bd) {
+            b.Display();
+            System.out.println("Luas: " + b.hitungLuas());
+            System.out.println("Keliling: " + b.hitungKeliling());
+            System.out.println("==================================");
+        }
+    }
+}
